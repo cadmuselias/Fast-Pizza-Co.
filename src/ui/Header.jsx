8d70972f25ff97dbb2753e5169d491835803f626
cadmuselias <emekonnen333@gmail.com>
2026-01-06 23:18:54 +0300

    Fix geolocation Immer bug and add hidden position input to order form

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+
 import SearchOrder from "../features/order/SearchOrder";
 import UserName from "../features/user/UserName";
 
@@ -8,7 +8,7 @@ function Header() {
     <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-400 px-4 py-3 uppercase tracking-widest sm:px-6">
       <Link to="/" className="flex items-center gap-2">
         {/* <img src={logo} alt="Fast Pizza Logo" className="max-h-10 w-auto" /> */}
-        <span className="font-semibold">Fast React Pizza Co.</span>
+        <span className="font-semibold">🍕 Fast React Pizza Co.</span>
       </Link>
       <SearchOrder />
       <UserName />
